23d96c9220007d3f07180021a3c20011282757d5
Alexey Pechnikov <pechnikov@mobigroup.ru>
2021-03-20 01:12:40 +0700

    Add missed AR files

--- Minahasa/Minahasa.apple.sh
+++ Minahasa/Minahasa.apple.sh
@@ -3,7 +3,7 @@
 usdzconvert \
     Minahasa.apple.glb \
     -m Material.GEBCO12_47_Reservoir -diffuseColor 0.799,0.421,0.05 -opacity 0.8 -metallic 0 -roughness 1 \
-    -m Material.measurements_temp_new -diffuseColor "measuments_temp_large".png \
+    -m Material.measurements_temp_new -diffuseColor measuments_temp_large.png \
     -m Material.COPERNICUS_S2_Lahendong_50m_32651 -diffuseColor COPERNICUS_S2_Lahendong_50m_32651.png -opacity 0.5 -metallic 0 -roughness 1 \
     -m Material.COPERNICUS_S2_Lahendong_50m_32651.back -diffuseColor 0.043,0.325,0.271 -opacity 0.1 -metallic 0 -roughness 1 \
     -metersPerUnit 0.5 \
